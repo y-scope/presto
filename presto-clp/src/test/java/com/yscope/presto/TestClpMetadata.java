311430df8636a3bed32d1a24e1a1c3f7c7ab4d41
wraymo <raywangv@gmail.com>
2024-06-09 18:18:45 -0400

    modify logs and add a test case

--- presto-clp/src/test/java/com/yscope/presto/TestClpMetadata.java
+++ presto-clp/src/test/java/com/yscope/presto/TestClpMetadata.java
@@ -38,7 +38,8 @@ public class TestClpMetadata
     @BeforeMethod
     public void setUp()
     {
-        ClpConfig config = new ClpConfig().setClpArchiveDir("src/test/resources/clp_archive");
+        ClpConfig config =
+                new ClpConfig().setClpArchiveDir("src/test/resources/clp_archive").setPolymorphicTypeEnabled(true);
         metadata = new ClpMetadata(new ClpClient(config));
     }
 
@@ -63,13 +64,87 @@ public class TestClpMetadata
                 (ClpTableHandle) metadata.getTableHandle(SESSION, new SchemaTableName("default", "test_1_table"));
         ConnectorTableMetadata tableMetadata = metadata.getTableMetadata(SESSION, clpTableHandle);
         HashSet<ColumnMetadata> columnMetadata = new HashSet<>();
-        columnMetadata.add(ColumnMetadata.builder().setName("a").setType(IntegerType.INTEGER).setNullable(true).build());
-        columnMetadata.add(ColumnMetadata.builder().setName("b").setType(DoubleType.DOUBLE).setNullable(true).build());
-        columnMetadata.add(ColumnMetadata.builder().setName("c.d").setType(BooleanType.BOOLEAN).setNullable(true).build());
-        columnMetadata.add(ColumnMetadata.builder().setName("c.e").setType(VarcharType.VARCHAR).setNullable(true).build());
-        columnMetadata.add(ColumnMetadata.builder().setName("a").setType(VarcharType.VARCHAR).setNullable(true).build());
-        columnMetadata.add(ColumnMetadata.builder().setName("b").setType(VarcharType.VARCHAR).setNullable(true).build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("a_integer")
+                .setType(IntegerType.INTEGER)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("b_double")
+                .setType(DoubleType.DOUBLE)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("c.d")
+                .setType(BooleanType.BOOLEAN)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("c.e")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("a_varchar")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("b_varchar")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
         columnMetadata.add(ColumnMetadata.builder().setName("c").setType(DoubleType.DOUBLE).setNullable(true).build());
+        assertEquals(columnMetadata, new HashSet<>(tableMetadata.getColumns()));
+    }
+
+    @Test
+    public void testGetTable2Metadata()
+    {
+        ClpTableHandle clpTableHandle =
+                (ClpTableHandle) metadata.getTableHandle(SESSION, new SchemaTableName("default", "test_2_table"));
+        ConnectorTableMetadata tableMetadata = metadata.getTableMetadata(SESSION, clpTableHandle);
+        HashSet<ColumnMetadata> columnMetadata = new HashSet<>();
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("a_double")
+                .setType(DoubleType.DOUBLE)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("b.c")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("b_integer")
+                .setType(IntegerType.INTEGER)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("c.d")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("c.e")
+                .setType(BooleanType.BOOLEAN)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("a_varchar")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("b_varchar")
+                .setType(VarcharType.VARCHAR)
+                .setNullable(true)
+                .build());
+        columnMetadata.add(ColumnMetadata.builder()
+                .setName("c")
+                .setType(BooleanType.BOOLEAN)
+                .setNullable(true)
+                .build());
         assertEquals(columnMetadata, new HashSet<>(tableMetadata.getColumns()));
     }
 }
