c089cdb5a998dabaa643e8a2c21352799a961371
wraymo <raywangv@gmail.com>
2025-01-02 09:28:08 -0500

    add support for reading metadata from mysql

--- presto-clp/src/main/java/com/yscope/presto/ClpConfig.java
+++ presto-clp/src/main/java/com/yscope/presto/ClpConfig.java
@@ -20,6 +20,12 @@ public class ClpConfig
     private String clpExecutablePath;
     private String clpArchiveDir;
     private boolean polymorphicTypeEnabled;
+    private String metadataDbUrl;
+    private String metadataDbUser;
+    private String metadataDbPassword;
+    private String metadataTablePrefix;
+    private long metadataRefreshInterval;
+    private long metadataExpireInterval;
 
     public String getClpExecutablePath()
     {
@@ -54,6 +60,78 @@ public class ClpConfig
     public ClpConfig setPolymorphicTypeEnabled(boolean polymorphicTypeEnabled)
     {
         this.polymorphicTypeEnabled = polymorphicTypeEnabled;
+        return this;
+    }
+
+    public String getMetadataDbUrl()
+    {
+        return metadataDbUrl;
+    }
+
+    @Config("metadata-db-url")
+    public ClpConfig setMetadataDbUrl(String metadataDbUrl)
+    {
+        this.metadataDbUrl = metadataDbUrl;
+        return this;
+    }
+
+    public String getMetadataDbUser()
+    {
+        return metadataDbUser;
+    }
+
+    @Config("metadata-db-user")
+    public ClpConfig setMetadataDbUser(String metadataDbUser)
+    {
+        this.metadataDbUser = metadataDbUser;
+        return this;
+    }
+
+    public String getMetadataDbPassword()
+    {
+        return metadataDbPassword;
+    }
+
+    @Config("metadata-db-password")
+    public ClpConfig setMetadataDbPassword(String metadataDbPassword)
+    {
+        this.metadataDbPassword = metadataDbPassword;
+        return this;
+    }
+
+    public String getMetadataTablePrefix()
+    {
+        return metadataTablePrefix;
+    }
+
+    @Config("metadata-table-prefix")
+    public ClpConfig setMetadataTablePrefix(String metadataTablePrefix)
+    {
+        this.metadataTablePrefix = metadataTablePrefix;
+        return this;
+    }
+
+    public long getMetadataRefreshInterval()
+    {
+        return metadataRefreshInterval;
+    }
+
+    @Config("metadata-refresh-interval")
+    public ClpConfig setMetadataRefreshInterval(long metadataRefreshInterval)
+    {
+        this.metadataRefreshInterval = metadataRefreshInterval;
+        return this;
+    }
+
+    public long getMetadataExpireInterval()
+    {
+        return metadataExpireInterval;
+    }
+
+    @Config("metadata-expire-interval")
+    public ClpConfig setMetadataExpireInterval(long metadataExpireInterval)
+    {
+        this.metadataExpireInterval = metadataExpireInterval;
         return this;
     }
 }
