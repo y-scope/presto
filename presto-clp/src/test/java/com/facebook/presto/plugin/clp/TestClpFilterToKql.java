b6ee2ea0eb9e720dd9bf2f80f8e0c31d2f3a215d
wraymo <raywangv@gmail.com>
2025-06-20 15:05:50 -0400

    fix inconsistent variable name and remove Optional from test cases

--- presto-clp/src/test/java/com/facebook/presto/plugin/clp/TestClpFilterToKql.java
+++ presto-clp/src/test/java/com/facebook/presto/plugin/clp/TestClpFilterToKql.java
@@ -32,23 +32,23 @@ public class TestClpFilterToKql
         SessionHolder sessionHolder = new SessionHolder();
 
         // Exact match
-        testFilter("city.Name = 'hello world'", Optional.of("city.Name: \"hello world\""), Optional.empty(), sessionHolder);
-        testFilter("'hello world' = city.Name", Optional.of("city.Name: \"hello world\""), Optional.empty(), sessionHolder);
+        testFilter("city.Name = 'hello world'", "city.Name: \"hello world\"", null, sessionHolder);
+        testFilter("'hello world' = city.Name", "city.Name: \"hello world\"", null, sessionHolder);
 
         // Like predicates that are transformed into substring match
-        testFilter("city.Name like 'hello%'", Optional.of("city.Name: \"hello*\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like '%hello'", Optional.of("city.Name: \"*hello\""), Optional.empty(), sessionHolder);
+        testFilter("city.Name like 'hello%'", "city.Name: \"hello*\"", null, sessionHolder);
+        testFilter("city.Name like '%hello'", "city.Name: \"*hello\"", null, sessionHolder);
 
         // Like predicates that are transformed into CARDINALITY(SPLIT(x, 'some string', 2)) = 2 form, and they are not pushed down for now
-        testFilter("city.Name like '%hello%'", Optional.empty(), Optional.of("city.Name like '%hello%'"), sessionHolder);
+        testFilter("city.Name like '%hello%'", null, "city.Name like '%hello%'", sessionHolder);
 
         // Like predicates that are kept in the original forms
-        testFilter("city.Name like 'hello_'", Optional.of("city.Name: \"hello?\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like '_hello'", Optional.of("city.Name: \"?hello\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like 'hello_w%'", Optional.of("city.Name: \"hello?w*\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like '%hello_w'", Optional.of("city.Name: \"*hello?w\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like 'hello%world'", Optional.of("city.Name: \"hello*world\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name like 'hello%wor%ld'", Optional.of("city.Name: \"hello*wor*ld\""), Optional.empty(), sessionHolder);
+        testFilter("city.Name like 'hello_'", "city.Name: \"hello?\"", null, sessionHolder);
+        testFilter("city.Name like '_hello'", "city.Name: \"?hello\"", null, sessionHolder);
+        testFilter("city.Name like 'hello_w%'", "city.Name: \"hello?w*\"", null, sessionHolder);
+        testFilter("city.Name like '%hello_w'", "city.Name: \"*hello?w\"", null, sessionHolder);
+        testFilter("city.Name like 'hello%world'", "city.Name: \"hello*world\"", null, sessionHolder);
+        testFilter("city.Name like 'hello%wor%ld'", "city.Name: \"hello*wor*ld\"", null, sessionHolder);
     }
 
     @Test
@@ -56,14 +56,14 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("substr(city.Name, 1, 2) = 'he'", Optional.of("city.Name: \"he*\""), Optional.empty(), sessionHolder);
-        testFilter("substr(city.Name, 5, 2) = 'he'", Optional.of("city.Name: \"????he*\""), Optional.empty(), sessionHolder);
-        testFilter("substr(city.Name, 5) = 'he'", Optional.of("city.Name: \"????he\""), Optional.empty(), sessionHolder);
-        testFilter("substr(city.Name, -2) = 'he'", Optional.of("city.Name: \"*he\""), Optional.empty(), sessionHolder);
+        testFilter("substr(city.Name, 1, 2) = 'he'", "city.Name: \"he*\"", null, sessionHolder);
+        testFilter("substr(city.Name, 5, 2) = 'he'", "city.Name: \"????he*\"", null, sessionHolder);
+        testFilter("substr(city.Name, 5) = 'he'", "city.Name: \"????he\"", null, sessionHolder);
+        testFilter("substr(city.Name, -2) = 'he'", "city.Name: \"*he\"", null, sessionHolder);
 
         // Invalid substring index is not pushed down
-        testFilter("substr(city.Name, 1, 5) = 'he'", Optional.empty(), Optional.of("substr(city.Name, 1, 5) = 'he'"), sessionHolder);
-        testFilter("substr(city.Name, -5) = 'he'", Optional.empty(), Optional.of("substr(city.Name, -5) = 'he'"), sessionHolder);
+        testFilter("substr(city.Name, 1, 5) = 'he'", null, "substr(city.Name, 1, 5) = 'he'", sessionHolder);
+        testFilter("substr(city.Name, -5) = 'he'", null, "substr(city.Name, -5) = 'he'", sessionHolder);
     }
 
     @Test
@@ -71,20 +71,20 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("fare > 0", Optional.of("fare > 0"), Optional.empty(), sessionHolder);
-        testFilter("fare >= 0", Optional.of("fare >= 0"), Optional.empty(), sessionHolder);
-        testFilter("fare < 0", Optional.of("fare < 0"), Optional.empty(), sessionHolder);
-        testFilter("fare <= 0", Optional.of("fare <= 0"), Optional.empty(), sessionHolder);
-        testFilter("fare = 0", Optional.of("fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("fare != 0", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("fare <> 0", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("0 < fare", Optional.of("fare > 0"), Optional.empty(), sessionHolder);
-        testFilter("0 <= fare", Optional.of("fare >= 0"), Optional.empty(), sessionHolder);
-        testFilter("0 > fare", Optional.of("fare < 0"), Optional.empty(), sessionHolder);
-        testFilter("0 >= fare", Optional.of("fare <= 0"), Optional.empty(), sessionHolder);
-        testFilter("0 = fare", Optional.of("fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("0 != fare", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("0 <> fare", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
+        testFilter("fare > 0", "fare > 0", null, sessionHolder);
+        testFilter("fare >= 0", "fare >= 0", null, sessionHolder);
+        testFilter("fare < 0", "fare < 0", null, sessionHolder);
+        testFilter("fare <= 0", "fare <= 0", null, sessionHolder);
+        testFilter("fare = 0", "fare: 0", null, sessionHolder);
+        testFilter("fare != 0", "NOT fare: 0", null, sessionHolder);
+        testFilter("fare <> 0", "NOT fare: 0", null, sessionHolder);
+        testFilter("0 < fare", "fare > 0", null, sessionHolder);
+        testFilter("0 <= fare", "fare >= 0", null, sessionHolder);
+        testFilter("0 > fare", "fare < 0", null, sessionHolder);
+        testFilter("0 >= fare", "fare <= 0", null, sessionHolder);
+        testFilter("0 = fare", "fare: 0", null, sessionHolder);
+        testFilter("0 != fare", "NOT fare: 0", null, sessionHolder);
+        testFilter("0 <> fare", "NOT fare: 0", null, sessionHolder);
     }
 
     @Test
@@ -92,23 +92,23 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("fare > 0 OR city.Name like 'b%'", Optional.of("(fare > 0 OR city.Name: \"b*\")"), Optional.empty(), sessionHolder);
+        testFilter("fare > 0 OR city.Name like 'b%'", "(fare > 0 OR city.Name: \"b*\")", null, sessionHolder);
         testFilter(
                 "lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1",
-                Optional.empty(),
-                Optional.of("(lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1)"),
+                null,
+                "(lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1)",
                 sessionHolder);
 
         // Multiple ORs
         testFilter(
                 "fare > 0 OR city.Name like 'b%' OR lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1",
-                Optional.empty(),
-                Optional.of("fare > 0 OR city.Name like 'b%' OR lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1"),
+                null,
+                "fare > 0 OR city.Name like 'b%' OR lower(city.Region.Name) = 'hello world' OR city.Region.Id != 1",
                 sessionHolder);
         testFilter(
                 "fare > 0 OR city.Name like 'b%' OR city.Region.Id != 1",
-                Optional.of("((fare > 0 OR city.Name: \"b*\") OR NOT city.Region.Id: 1)"),
-                Optional.empty(),
+                "((fare > 0 OR city.Name: \"b*\") OR NOT city.Region.Id: 1)",
+                null,
                 sessionHolder);
     }
 
@@ -117,23 +117,23 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("fare > 0 AND city.Name like 'b%'", Optional.of("(fare > 0 AND city.Name: \"b*\")"), Optional.empty(), sessionHolder);
+        testFilter("fare > 0 AND city.Name like 'b%'", "(fare > 0 AND city.Name: \"b*\")", null, sessionHolder);
         testFilter(
                 "lower(city.Region.Name) = 'hello world' AND city.Region.Id != 1",
-                Optional.of("(NOT city.Region.Id: 1)"),
-                Optional.of("lower(city.Region.Name) = 'hello world'"),
+                "(NOT city.Region.Id: 1)",
+                "lower(city.Region.Name) = 'hello world'",
                 sessionHolder);
 
         // Multiple ANDs
         testFilter(
                 "fare > 0 AND city.Name like 'b%' AND lower(city.Region.Name) = 'hello world' AND city.Region.Id != 1",
-                Optional.of("(((fare > 0 AND city.Name: \"b*\")) AND NOT city.Region.Id: 1)"),
-                Optional.of("(lower(city.Region.Name) = 'hello world')"),
+                "(((fare > 0 AND city.Name: \"b*\")) AND NOT city.Region.Id: 1)",
+                "(lower(city.Region.Name) = 'hello world')",
                 sessionHolder);
         testFilter(
                 "fare > 0 AND city.Name like '%b%' AND lower(city.Region.Name) = 'hello world' AND city.Region.Id != 1",
-                Optional.of("(((fare > 0)) AND NOT city.Region.Id: 1)"),
-                Optional.of("city.Name like '%b%' AND lower(city.Region.Name) = 'hello world'"),
+                "(((fare > 0)) AND NOT city.Region.Id: 1)",
+                "city.Name like '%b%' AND lower(city.Region.Name) = 'hello world'",
                 sessionHolder);
     }
 
@@ -142,19 +142,19 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("city.Region.Name NOT LIKE 'hello%'", Optional.of("NOT city.Region.Name: \"hello*\""), Optional.empty(), sessionHolder);
-        testFilter("NOT (city.Region.Name LIKE 'hello%')", Optional.of("NOT city.Region.Name: \"hello*\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name != 'hello world'", Optional.of("NOT city.Name: \"hello world\""), Optional.empty(), sessionHolder);
-        testFilter("city.Name <> 'hello world'", Optional.of("NOT city.Name: \"hello world\""), Optional.empty(), sessionHolder);
-        testFilter("NOT (city.Name = 'hello world')", Optional.of("NOT city.Name: \"hello world\""), Optional.empty(), sessionHolder);
-        testFilter("fare != 0", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("fare <> 0", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("NOT (fare = 0)", Optional.of("NOT fare: 0"), Optional.empty(), sessionHolder);
+        testFilter("city.Region.Name NOT LIKE 'hello%'", "NOT city.Region.Name: \"hello*\"", null, sessionHolder);
+        testFilter("NOT (city.Region.Name LIKE 'hello%')", "NOT city.Region.Name: \"hello*\"", null, sessionHolder);
+        testFilter("city.Name != 'hello world'", "NOT city.Name: \"hello world\"", null, sessionHolder);
+        testFilter("city.Name <> 'hello world'", "NOT city.Name: \"hello world\"", null, sessionHolder);
+        testFilter("NOT (city.Name = 'hello world')", "NOT city.Name: \"hello world\"", null, sessionHolder);
+        testFilter("fare != 0", "NOT fare: 0", null, sessionHolder);
+        testFilter("fare <> 0", "NOT fare: 0", null, sessionHolder);
+        testFilter("NOT (fare = 0)", "NOT fare: 0", null, sessionHolder);
 
         // Multiple NOTs
-        testFilter("NOT (NOT fare = 0)", Optional.of("NOT NOT fare: 0"), Optional.empty(), sessionHolder);
-        testFilter("NOT (fare = 0 AND city.Name = 'hello world')", Optional.of("NOT (fare: 0 AND city.Name: \"hello world\")"), Optional.empty(), sessionHolder);
-        testFilter("NOT (fare = 0 OR city.Name = 'hello world')", Optional.of("NOT (fare: 0 OR city.Name: \"hello world\")"), Optional.empty(), sessionHolder);
+        testFilter("NOT (NOT fare = 0)", "NOT NOT fare: 0", null, sessionHolder);
+        testFilter("NOT (fare = 0 AND city.Name = 'hello world')", "NOT (fare: 0 AND city.Name: \"hello world\")", null, sessionHolder);
+        testFilter("NOT (fare = 0 OR city.Name = 'hello world')", "NOT (fare: 0 OR city.Name: \"hello world\")", null, sessionHolder);
     }
 
     @Test
@@ -162,7 +162,7 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("city.Name IN ('hello world', 'hello world 2')", Optional.of("(city.Name: \"hello world\" OR city.Name: \"hello world 2\")"), Optional.empty(), sessionHolder);
+        testFilter("city.Name IN ('hello world', 'hello world 2')", "(city.Name: \"hello world\" OR city.Name: \"hello world 2\")", null, sessionHolder);
     }
 
     @Test
@@ -170,9 +170,9 @@ public class TestClpFilterToKql
     {
         SessionHolder sessionHolder = new SessionHolder();
 
-        testFilter("city.Name IS NULL", Optional.of("NOT city.Name: *"), Optional.empty(), sessionHolder);
-        testFilter("city.Name IS NOT NULL", Optional.of("NOT NOT city.Name: *"), Optional.empty(), sessionHolder);
-        testFilter("NOT (city.Name IS NULL)", Optional.of("NOT NOT city.Name: *"), Optional.empty(), sessionHolder);
+        testFilter("city.Name IS NULL", "NOT city.Name: *", null, sessionHolder);
+        testFilter("city.Name IS NOT NULL", "NOT NOT city.Name: *", null, sessionHolder);
+        testFilter("NOT (city.Name IS NULL)", "NOT NOT city.Name: *", null, sessionHolder);
     }
 
     @Test
@@ -182,30 +182,30 @@ public class TestClpFilterToKql
 
         testFilter(
                 "(fare > 0 OR city.Name like 'b%') AND (lower(city.Region.Name) = 'hello world' OR city.Name IS NULL)",
-                Optional.of("((fare > 0 OR city.Name: \"b*\"))"),
-                Optional.of("(lower(city.Region.Name) = 'hello world' OR city.Name IS NULL)"),
+                "((fare > 0 OR city.Name: \"b*\"))",
+                "(lower(city.Region.Name) = 'hello world' OR city.Name IS NULL)",
                 sessionHolder);
         testFilter(
                 "city.Region.Id = 1 AND (fare > 0 OR city.Name NOT like 'b%') AND (lower(city.Region.Name) = 'hello world' OR city.Name IS NULL)",
-                Optional.of("((city.Region.Id: 1 AND (fare > 0 OR NOT city.Name: \"b*\")))"),
-                Optional.of("lower(city.Region.Name) = 'hello world' OR city.Name IS NULL"),
+                "((city.Region.Id: 1 AND (fare > 0 OR NOT city.Name: \"b*\")))",
+                "lower(city.Region.Name) = 'hello world' OR city.Name IS NULL",
                 sessionHolder);
     }
 
-    private void testFilter(String sqlExpression, Optional<String> expectedKqlExpression, Optional<String> expectedRemainingExpression, SessionHolder sessionHolder)
+    private void testFilter(String sqlExpression, String expectedKqlExpression, String expectedRemainingExpression, SessionHolder sessionHolder)
     {
         RowExpression pushDownExpression = getRowExpression(sqlExpression, sessionHolder);
         ClpExpression clpExpression = pushDownExpression.accept(new ClpFilterToKqlConverter(standardFunctionResolution, functionAndTypeManager, variableToColumnHandleMap), null);
         Optional<String> kqlExpression = clpExpression.getDefinition();
         Optional<RowExpression> remainingExpression = clpExpression.getRemainingExpression();
-        if (expectedKqlExpression.isPresent()) {
+        if (expectedKqlExpression != null) {
             assertTrue(kqlExpression.isPresent());
-            assertEquals(kqlExpression.get(), expectedKqlExpression.get());
+            assertEquals(kqlExpression.get(), expectedKqlExpression);
         }
 
-        if (expectedRemainingExpression.isPresent()) {
+        if (expectedRemainingExpression != null) {
             assertTrue(remainingExpression.isPresent());
-            assertEquals(remainingExpression.get(), getRowExpression(expectedRemainingExpression.get(), sessionHolder));
+            assertEquals(remainingExpression.get(), getRowExpression(expectedRemainingExpression, sessionHolder));
         }
         else {
             assertFalse(remainingExpression.isPresent());
