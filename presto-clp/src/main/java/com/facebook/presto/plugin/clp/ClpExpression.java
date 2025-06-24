537c424e2b62fba41d7e53c61a463680323f875c
wraymo <raywangv@gmail.com>
2025-06-24 11:23:51 -0400

    fix a name in comments

--- presto-clp/src/main/java/com/facebook/presto/plugin/clp/ClpExpression.java
+++ presto-clp/src/main/java/com/facebook/presto/plugin/clp/ClpExpression.java
@@ -19,8 +19,9 @@ import java.util.Optional;
 
 /**
  * Represents the result of converting a Presto RowExpression into a CLP-compatible KQL query. In
- * every case, `kqlQuery` represents the part of the RowExpression that could be converted to a
- * KQL expression, and `remainingExpression` represents the part that could not be converted.
+ * every case, `pushDownExpression` represents the part of the RowExpression that could be
+ * converted to a KQL expression, and `remainingExpression` represents the part that could not be
+ * converted.
  */
 public class ClpExpression
 {
