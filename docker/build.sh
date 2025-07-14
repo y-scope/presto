4ca014c6b3118e9376243c832151e09cc96939bf
Xiaochong Wei <xiaochong.wei@yscope.com>
2025-07-14 10:03:06 -0400

    Fix

--- docker/build.sh
+++ docker/build.sh
@@ -17,19 +17,22 @@ VERSION=$1; shift
 TAG="${TAG:-latest}"
 IMAGE_NAME="${IMAGE_NAME:-presto}"
 REG_ORG="${REG_ORG:-docker.io/prestodb}"
+PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64,linux/ppc64le}"
+TMP_IIDFILE=$(mktemp)
+IIDFILE="${IIDFILE:-$TMP_IIDFILE}"
 PUBLISH="${PUBLISH:-false}"
+BUILDER="${BUILDER:-container}"
 
-# Compose full image name with tag
-FULL_IMAGE_NAME="${REG_ORG}/${IMAGE_NAME}:${TAG}"
-
-# Build image using regular docker build
-docker build \
+export BUILDX_NO_DEFAULT_ATTESTATIONS=""
+# If PUBLISH=false, images only stores in local cache, otherwise they are pushed to th container registry
+docker buildx build --builder="${BUILDER}" --iidfile "${IIDFILE}" \
     --build-arg="PRESTO_VERSION=${VERSION}" \
     --build-arg="JMX_PROMETHEUS_JAVAAGENT_VERSION=0.20.0" \
-    -t "${FULL_IMAGE_NAME}" \
-    -f Dockerfile .
+    --output "type=image,name=${REG_ORG}/${IMAGE_NAME},push-by-digest=true,name-canonical=true,push=${PUBLISH}" \
+    --platform "${PLATFORMS}" -f Dockerfile .
 
-# Optionally push the image
-if [[ "$PUBLISH" == "true" ]]; then
-    docker push "${FULL_IMAGE_NAME}"
+if [[ "$PUBLISH" = "true" ]]; then
+    # This only happens when push=true, since push-by-digest=true in the above build step, need to tag the images explicitly
+    docker buildx imagetools create --builder="${BUILDER}" \
+    -t "${REG_ORG}/${IMAGE_NAME}:${TAG}" "${REG_ORG}/${IMAGE_NAME}@$(cat "$IIDFILE")"
 fi
